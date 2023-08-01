510762b335e4d89d849854456120fd9875c7d6c5
TH-Raju <rajukhan894200@gmail.com>
2023-08-02 00:38:16 +0600

    [Done] access token setup

--- dist/app/modules/cow/cow.route.js
+++ dist/app/modules/cow/cow.route.js
@@ -11,7 +11,7 @@ const user_interface_1 = require("../user/user.interface");
 const router = express_1.default.Router();
 router.patch('/:id', (0, auth_1.default)(user_interface_1.UserRole.Seller), cow_controller_1.CowController.updatedCow);
 router.get('/:id', (0, auth_1.default)(user_interface_1.UserRole.Admin, user_interface_1.UserRole.Buyer, user_interface_1.UserRole.Seller), cow_controller_1.CowController.getSingleCow);
-router.delete('/:id', (0, auth_1.default)(user_interface_1.UserRole.Admin, user_interface_1.UserRole.Buyer, user_interface_1.UserRole.Seller), (0, auth_1.default)(user_interface_1.UserRole.Seller), cow_controller_1.CowController.deleteCow);
+router.delete('/:id', (0, auth_1.default)(user_interface_1.UserRole.Seller), cow_controller_1.CowController.deleteCow);
 router.get('/', (0, auth_1.default)(user_interface_1.UserRole.Admin, user_interface_1.UserRole.Buyer, user_interface_1.UserRole.Seller), cow_controller_1.CowController.getAllCows);
 router.post('/', (0, auth_1.default)(user_interface_1.UserRole.Seller), cow_controller_1.CowController.createCow);
 exports.CowRoutes = router;
