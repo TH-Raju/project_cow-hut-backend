a434e70d0748a50cdfe40edd140215c0dd8bd317
TH-Raju <rajukhan894200@gmail.com>
2023-08-01 22:00:53 +0600

    [Done] single orders

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import { OrderController } from './order.controller'
 const router = express.Router()
 
-router.get('/', OrderController.getAllOrders)
 router.get('/:id', OrderController.getSingleOrder)
+router.get('/', OrderController.getAllOrders)
 router.post('/', OrderController.createOrder)
 
 export const OrderRoutes = router
